480f5f54257852912528ae1d88f67c97a6e1a782
Jay Lee <thecodingdelight@gmail.com>
2017-08-15 19:39:20 +0900

    Create exercise1_4.c

---
+++ Chapter-one/src/exercise1_4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    // Heading
+    printf("Degrees Celsius    Fahrenheit \n");
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        // Changed order from previous exercise
+        printf("%3.0f \t\t %6.1f\n",  celsius, fahr);
+        fahr = fahr + step;
+    }
+    return 0;
+}
