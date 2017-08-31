96bae5c4e44c44637ac629ccee93ddcaf0cfe2b8
Jay Lee <thecodingdelight@gmail.com>
2017-08-31 22:11:46 +0900

    Update exercise1_12.c

--- Chapter-one/src/exercise1_12.c
+++ Chapter-one/src/exercise1_12.c
@@ -1,7 +1,7 @@
 void one_word_per_line() {
     int c;
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == ' \t') {
+        if (c == ' ' || c == '\t') {
             putchar('\n');
         } else {
             putchar(c);
